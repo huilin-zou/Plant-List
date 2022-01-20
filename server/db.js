dcb9d1190ebcc7331ec0f12aab4dbef61bbc3a6a
Huilin <56774880+huilin-zou@users.noreply.github.com>
2022-01-19 23:06:14 -0500

    Update db.js

--- server/db.js
+++ server/db.js
@@ -2,9 +2,7 @@ const Pool=require("pg").Pool;
 const bcrypt = require("bcryptjs"); // hash password
 const pool=new Pool({
     user:"postgres",
-    password:"zhl0818",
-    host:"localhost",
-    port:5432,
+    
     database:"pernplants"
 });
 
